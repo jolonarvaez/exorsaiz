11c28ff5ce41f6f37f9e599946d8ebf7c6081243
migsmo <97015261+migsmo@users.noreply.github.com>
2023-03-06 22:38:26 +0800

    ADD scene and text transition in game controller script

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public StoryScene currentScene;
+    public SpeechBarController speechBar;
+    public BackgroundController backgroundController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speechBar.PlayScene(currentScene);
+        backgroundController.SetImage(currentScene.background);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if(speechBar.IsCompleted())
+            {
+                if(speechBar.IsLastSentence())
+                {
+                    currentScene = currentScene.nextScene;
+                    speechBar.PlayScene(currentScene);
+                    backgroundController.SwitchImage(currentScene.background);
+                } else
+                {
+                    speechBar.PlayNextSentence();
+
+                }
+            }
+        }
     }
 }
