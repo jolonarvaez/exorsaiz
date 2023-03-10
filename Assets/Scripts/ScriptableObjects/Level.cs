e6462e22e771773f320df2563270c858a8b34595
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2023-03-10 19:57:25 +0800

    added level select grid

--- Assets/Scripts/ScriptableObjects/Level.cs
+++ Assets/Scripts/ScriptableObjects/Level.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Level", menuName = "Card")] 
+[CreateAssetMenu(fileName = "New Level", menuName = "Level")] 
 public class Level : ScriptableObject
 {
     public int levelNumber;
