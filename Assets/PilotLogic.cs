1a4220bf438575dd543fcef2bb7331f3c5cede06
Richard Narvaez <75784101+RANarvz@users.noreply.github.com>
2023-03-11 13:46:15 +0800

    MediaPipe Plugins (3)

--- Assets/PilotLogic.cs
+++ Assets/PilotLogic.cs
@@ -5,16 +5,27 @@ using UnityEngine.UI;
 using TMPro;
 public class PilotLogic : MonoBehaviour
 {
+    public TextMeshProUGUI Timer;
     public TextMeshProUGUI ExerciseLabel;
     public Image SyncBar;
     public Image SideBarL;
     public Image SideBarR;
     public int SyncPercentage;
+    public float ExerciseTimer = 0f;
     public string ExerciseName = "Static Lunge";
+    private float CurrentTime = 0f;
+    private float RestTimer = 10.3f;
+    private bool ExerciseDone = false;
+    private bool getReady = true;
+    private bool startExercise = false;
+    private  string[] ExerciseList = new string[8] { "Wall Sit", "Sumo Squat", "Static Lunge(L)", "Static Lunge(R)",
+                                      "Glute Bridge", "Single Leg Glute Bridge(L)", "Single Leg Glute Bridge(R)",
+                                      "Straight Bridge"};
+    private int currExercise=0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        setLabel(ExerciseList[currExercise]);
     }
 
     // Update is called once per frame
@@ -23,6 +34,10 @@ public class PilotLogic : MonoBehaviour
         getSyncBar();
         getLabel();
         setBar();
+        if (currExercise < ExerciseList.Length)
+        {
+            SetExerciseTimer();
+        }
     }
 
     public void getSyncBar()
@@ -30,9 +45,9 @@ public class PilotLogic : MonoBehaviour
         SyncBar.fillAmount = (float)SyncPercentage / 100;
     }
 
-    public void setLabel()
+    public void setLabel(string Label)
     {
-        ExerciseName = "Easy Side Planks - Left";
+        ExerciseName = Label;
     }
 
     public void getLabel()
@@ -58,5 +73,43 @@ public class PilotLogic : MonoBehaviour
             SideBarR.color = new Color32(238, 63, 63, 255);
         }
 
+    }
+
+    public void SetExerciseTimer()
+    {
+        Timer.text = CurrentTime.ToString("0");
+        if (!ExerciseDone)
+        {
+            if (getReady)
+            {
+                CurrentTime = 5f;
+                getReady = false;
+                setLabel("Get Ready");
+            }
+            if(!getReady && CurrentTime <=0 && !startExercise)
+            {
+                CurrentTime = ExerciseTimer + 0.3f;
+                startExercise = true;
+                setLabel(ExerciseList[currExercise]);
+            }
+            if (startExercise && CurrentTime <= 0)
+            {
+                startExercise = false;
+                ExerciseDone = true;
+                CurrentTime = RestTimer;
+                setLabel("Rest");
+            }
+        }
+        else
+        {
+            if (CurrentTime <= 0)
+            {
+                ExerciseDone = false;
+                CurrentTime = ExerciseTimer;
+                currExercise++;
+                getReady = true;
+            }
+        }
+        CurrentTime -= 1 * Time.deltaTime;
     }
 }
